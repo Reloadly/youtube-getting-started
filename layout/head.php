3268c516f4a2bcbc4896eee6606a721668479ee1
Junaid Maqsood <junaidmaqsood@live.com>
2020-06-09 15:42:13 +0500

    Layout Constructed

---
+++ layout/head.php
@@ -0,0 +1,10 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+?>
+<html>
+<head>
+    <title>Reloadly Youtube Demo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
---
+++ layout/header.php
@@ -0,0 +1,31 @@
+<body class="d-flex flex-column h-100">
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="/">Reloadly Youtube</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/countries.php">Countries</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/operators.php">Operators</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/discounts.php">Discounts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/promotions.php">Promotions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/transactions.php">Transactions</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
